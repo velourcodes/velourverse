09caf4ff74c85dd63b9af7c7b930a5f781680ad1
velourcodes <mohammadaffansiddiqi98@gmail.com>
2025-12-10 16:47:27 +0530

    Added better error handling by explicit conversion of mongoose doc to JSON

--- backend/src/models/subscriptions.model.js
+++ backend/src/models/subscriptions.model.js
@@ -1,15 +1,20 @@
 import mongoose from "mongoose";
-const subscriptionsSchema = new mongoose.Schema({
-    subscriber:
+const subscriptionsSchema = new mongoose.Schema(
     {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
+        subscriber: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
+        channel: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+        },
     },
-    channel:
     {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User"
-    }
-}, { timestamps: true });
+        toJSON: { virtuals: true, getters: true },
+        toObject: { virtuals: true, getters: true },
+    },
+    { timestamps: true }
+);
 
 export const Subsciption = mongoose.model("Subscription", subscriptionsSchema);

1cc4daef3ac3ff6a4a99b0dee81b3ca53f601518
velourcodes <mohammadaffansiddiqi98@gmail.com>
2025-12-27 00:30:36 +0530

    Renamed users and videos models to 'user' and 'video' for consistency & updated code of controller accordingly

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
-import { User } from "../models/users.model.js";
+import { User } from "../models/user.model.js";
 import {
     uploadOnCloudinary,
     deleteFromCloudinary,
